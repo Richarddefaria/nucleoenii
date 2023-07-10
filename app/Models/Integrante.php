9ba0a46da08f34f3e5db6e65c6d2ec39daeaf2d4
Richarddefaria <108709557+Richarddefaria@users.noreply.github.com>
2023-07-10 16:26:12 -0400

    Create Integrante.php

---
+++ app/Models/Integrante.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Integrante extends Model
+{
+    use HasFactory;
+
+    protected $table = 'integrantes';
+
+    // Agregar la relación "belongsTo" con el modelo de Usuario
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
